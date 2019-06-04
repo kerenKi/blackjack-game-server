91ffd598fc6365695fff767b843fde11d57ebe02
kerenKi <kkinberg13@gmail.com>
2019-06-05 00:08:36 +0200

    When user play his turn the next turn emits to all clients

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ const socketIo = require("socket.io");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRouter = require('./users/routes')
-const routing = require('./Game/routes')
+const { playersRouting, gameRouting } = require('./Game/routes')
+
 
 //Array of players playing the game:
 const players = []
 
+let current_turn = 0;
+let _turn = 0;
+
+//function that scan the players array and returns the id of the next player
+next_turn = () => {
+  _turn = current_turn++ % players.length;
+  console.log("next turn goes to: " , players[_turn]);
+  return players[_turn]
+}
+
 // Dispatch the array of players to all clients:
-const dispatcher = io => {
+const playersDispatcher = io => {
   return function dispatch(payload) {
     const action = {
       type: "PLAYERS_IN_GAME",
@@ -23,22 +34,39 @@ const dispatcher = io => {
   };
 };
 
+// Dispatch the player who plays next to all clients:
+const gameDispatcher = io => {
+  return function dispatch(payload) {
+    const action = {
+      type: "NEXT_TURN",
+      payload
+    };
+
+    io.emit("action", action);
+  };
+};
+
+
+
 // Listen
 const server = app.listen(port, () =>
   console.log("Express listens on port " + port + "!")
 );
 const io = socketIo.listen(server);
 
-const dispatch = dispatcher(io);
+const dispatchPlayers = playersDispatcher(io);
+const dispatchNextTurn = gameDispatcher(io);
 
 
-const gameRouter = routing(dispatch, players)
+const playersRouter = playersRouting(dispatchPlayers, players)
+const gameRouter = gameRouting(dispatchNextTurn, next_turn)
 
 
 // Use
 app.use(cors());
 app.use(bodyParser.json());
 app.use(userRouter)
+app.use(playersRouter)
 app.use(gameRouter)
 
 
@@ -54,8 +82,13 @@ io.on("connection", client => {
   console.log(client.id, "connects.");
 
   // Send action
-  dispatch(players)
+  dispatchPlayers(players)
 
   // Disconnect
   client.on("disconnect", () => console.log(client.id, "disconnects."));
 });
+
+
+
+
+
