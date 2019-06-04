ecf4b6defb51a162974b9471358cf24922de149a
kerenKi <kkinberg13@gmail.com>
2019-06-04 16:54:12 +0200

    moved create user route from app.js to /users/routes.js

--- app.js
+++ app.js
@@ -5,32 +5,18 @@ const port = 4000;
 const socketIo = require("socket.io");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const User = require("./users/model");
+const userRouter = require('./users/routes')
+
 
 // Use
 app.use(cors());
 app.use(bodyParser.json());
+app.use(userRouter)
+
 
 // Home
 app.get("/", (req, res) => {
   res.send("hello from home!");
-});
-
-// Create user
-app.post("/users", (req, res, next) => {
-  const user = {
-    name: req.body.name
-  };
-  User.create(user)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({
-          message: `User does not exist`
-        });
-      }
-      return res.status(201).send(user);
-    })
-    .catch(error => next(error));
 });
 
 // Listen
